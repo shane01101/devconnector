3f19cf3bd25631a52a74ca7fc82a793e44870b86
Shane Honanie <shaneh425@gmail.com>
2018-12-07 15:58:55 -0800

    Profile Reducer

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 import { Provider } from 'react-redux';
 import store from './store';
+import { clearCurrentProfile } from './actions/profileActions';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 
 import './App.css';
 
@@ -27,8 +29,9 @@ if (localStorage.jwtToken) {
 	const currentTime = Date.now() / 1000;
 	if (decoded.exp < currentTime) {
 		// Logout user
-		//store.dispatch(logoutUser());
-		// TODO: Clear current profile
+		store.dispatch(logoutUser());
+		// Clear current profile
+		store.dispatch(clearCurrentProfile());
 		// Redirect to login
 		window.location.href = '/login';
 	}
@@ -49,6 +52,11 @@ class App extends Component {
 								component={Register}
 							/>
 							<Route exact path="/login" component={Login} />
+							<Route
+								exact
+								path="/dashboard"
+								component={Dashboard}
+							/>
 						</div>
 						<Footer />
 					</div>
