5075bdf4c88c1237edc376cbe6f89031d7eb10f1
Shane Honanie <shaneh425@gmail.com>
2018-12-11 18:06:06 -0800

    Posts & Post Form Component

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,3 +1,5 @@
+import { ADD_POST } from '../actions/types';
+
 const intitalState = {
 	posts: [],
 	port: {},
@@ -6,6 +8,11 @@ const intitalState = {
 
 export default function(state = intitalState, action) {
 	switch (action.type) {
+		case ADD_POST:
+			return {
+				...state,
+				posts: [action.payload, ...state.posts]
+			};
 		default:
 			return state;
 	}
