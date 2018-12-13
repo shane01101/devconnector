f39d1c5b0b61c9e31cfbafeacb775a41fb4bbf55
Shane Honanie <shaneh425@gmail.com>
2018-12-13 14:18:44 -0800

    Comment Display & Delete / Clear Errors

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import PostItem from '../posts/PostItem';
 import CommentForm from './CommentForm';
+import CommentFeed from './CommentFeed';
 import Spinner from '../common/Spinner';
 import { getPost } from '../../actions/postActions';
 
@@ -23,6 +24,7 @@ class Post extends Component {
 				<div>
 					<PostItem post={post} showActions={false} />
 					<CommentForm postId={post._id} />
+					<CommentFeed postId={post._id} comments={post.comments} />
 				</div>
 			);
 		}
@@ -32,8 +34,8 @@ class Post extends Component {
 				<div className="container">
 					<div className="row">
 						<div className="col-md-12">
-							<Link to="/feed" className="btn brn-light mb-2">
-								Back to Feed
+							<Link to="/feed" className="btn btn-light mb-3">
+								Back To Feed
 							</Link>
 							{postContent}
 						</div>
